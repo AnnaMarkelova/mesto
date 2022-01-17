6f670a62a1b40daa4ba489dc3db46304934cb671
Anna Markelova <anytkamarkelova@yandex.ru>
2022-01-17 12:30:53 +0300

    исправляет ошибки в файле JS

--- index.js
+++ index.js
@@ -1,50 +1,34 @@
-let fullNameProfile = 'Жак-Ив Кусто';
-let statusProfile = 'Исследователь океана';
 let editBtn = document.querySelector('.profile__edit-btn');
 let closeBtn = document.querySelector('.popup__btn-close');
 let formPopUp = document.querySelector('.popup__container');
 let popUp = document.querySelector('.popup');
-
-function fillProfile (nameValue, statusValue) {
-  let profileName = document.querySelector('.profile__name');
-  let profileStatus = document.querySelector('.profile__status');
-  profileName.textContent = nameValue;
-  profileStatus.textContent = statusValue;
-}
-
-function fillPopUp (nameValue, statusValue) {
-  let inputName = document.querySelector('.popup__input_name');
-  let inputStatus = document.querySelector('.popup__input_status');
-  inputName.value = nameValue;
-  inputStatus.value = statusValue;
-}
+let profileName = document.querySelector('.profile__name');
+let profileStatus = document.querySelector('.profile__status');
+let inputName = document.querySelector('.popup__input_name');
+let inputStatus = document.querySelector('.popup__input_status');
 
 function closePopUp () {
-  let popUP = document.querySelector('.popup');
-  if (popUP.classList.contains('popup_opened')) {
-    popUP.classList.remove('popup_opened');
-  }
+  popUp.classList.remove('popup_opened');
 }
 
-fillProfile(fullNameProfile, statusProfile);
+function openPopUp () {
+  popUp.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputStatus.value = profileStatus.textContent;
+}
+
+function savePopUp () {
+  profileName.textContent = inputName.value;
+  profileStatus.textContent = inputStatus.value;
+  closePopUp();
+}
 
 formPopUp.addEventListener('submit', function (evt) {
   evt.preventDefault();
-  let inputName = document.querySelector('.popup__input_name');
-  let inputStatus = document.querySelector('.popup__input_status');
-  fullNameProfile = inputName.value;
-  statusProfile = inputStatus.value;
-  fillProfile (fullNameProfile, statusProfile);
-  closePopUp();
+  savePopUp();
 });
 
-editBtn.addEventListener('click', function () {
-  let popUP = document.querySelector('.popup');
-  if (!popUP.classList.contains('popup_opened')) {
-    popUP.classList.add('popup_opened');
-    fillPopUp(fullNameProfile, statusProfile);
-  }
-});
+editBtn.addEventListener('click', openPopUp);
 
 closeBtn.addEventListener('click', function () {
   closePopUp();
